63942974aa5f4c953125f5251771cc5e695e93c3
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-11-07 12:11:05 +0200

    Delete ReactJS/02.Components: Basic Idea/react-component-basics directory

--- ReactJS/02.Components: Basic Idea/react-component-basics/src/components/TaskListItem.js
+++
@@ -1,9 +0,0 @@
-const TaskListItem = (props) => {
-  if(props.person) {
-    console.log(props.person.name)
-  }
-
-  return <li style={{color: props.color}}>{props.children} - {props.person?.name}</li>;
-};
-
-export default TaskListItem;
